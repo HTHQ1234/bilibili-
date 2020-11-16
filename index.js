6e3749faf84caff89547c0c35fafc0a298195a9e
HTHQ1234 <851332710@qq.com>
2020-11-16 14:44:41 +0800

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const images = document.querySelectorAll('header > div >img');
+document.querySelector('header').addEventListener('mousemove', (e) => {
+  let percentage = e.clientX / window.outerWidth;
+  let offset = 10 * percentage;
+  let blur = 20;
+  for (let [index, image] of images.entries()) {
+    offset *= 1.3;
+    let blurValue = (Math.pow((index / images.length - percentage), 2) * blur);
+    image.style.setProperty('--offset', `${offset}px`);
+    image.style.setProperty('--blur', `${blurValue}px`);
+  }
+})
